9f2b7f9febea211a531f89dcffea601f0b760958
ojaohe-3 <ojaohe-3@student.ltu.se>
2021-03-17 00:44:58 +0100

    my god what have idone

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,17 @@ import * as WebSocket from "ws";
 import DeviceHandler from "./DeviceHandler";
 import app from "./app";
 
+declare interface SendFormat {
+  id: number;
+  name: string;
+  type: string;
+  baseType: string;
+  created: Date;
+  modifier: Date;
+  batteryLevel?: number;
+  value: string;
+}
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -26,17 +37,28 @@ const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws) => {
   console.log("connection");
-  DeviceHandler.Instance.ItemsAvailable.on("update", () =>
-  ws.send(JSON.stringify(DeviceHandler.Instance.Selected))
-);
+  DeviceHandler.Instance.ItemsAvailable.on("update", () => {
+    const selected = DeviceHandler.Instance.Selected;
+    const data: SendFormat = {
+      id: selected.id,
+      name: selected.name,
+      type: selected.type,
+      baseType: selected.baseType,
+      created: selected.created,
+      modifier: selected.modified,
+      batteryLevel: selected.properties.batteryLevel,
+      value: selected.properties.value,
+    };
+    console.log(data.value, "sent to client")
+    ws.send(JSON.stringify(data));
+  });
   ws.on("message", (data) => {
     console.log(data);
     DeviceHandler.Instance.selectDevice(+data);
-    
   });
 
   ws.on("close", () => {
-    console.log('closed connection')
+    console.log("closed connection");
     DeviceHandler.Instance.unSelect();
     DeviceHandler.Instance.ItemsAvailable.off("update");
   });
